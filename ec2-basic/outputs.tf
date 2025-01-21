c2e87184adbcd24fe491eff308dce6fd76579f36
Nimrod Jocsan Trejo Castro <nimo-moto@Mac-mini-de-Nimrod.local>
2025-01-20 19:27:51 -0600

    updated module output

--- ec2-basic/outputs.tf
+++ ec2-basic/outputs.tf
@@ -1,4 +1,4 @@
 output "instance_public_ips" {
-  description = "Public IP of the EC2 instances for ${var.environment}"
+  description = "Public IP of the EC2 instances"
   value       = module.ec2.instance_public_ips
 }
