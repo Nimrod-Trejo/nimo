d2fc0800436f8c2a815fbce2d2ed8850b10e42e4
Nimrod Jocsan Trejo Castro <nimo-moto@Mac-mini-de-Nimrod.local>
2025-01-20 19:31:30 -0600

    res vars root

---
+++ ec2-basic/variables.tf
@@ -0,0 +1,23 @@
+variable "aws_region" {
+  description = "AWS region"
+  default = "us-west-2"
+}
+
+variable "ami_id" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+}
+
+variable "instance_count" {
+  description = "Number of instances"
+  type        = number
+}
+variable "environment" {
+  description = "Environment name (dev, qa, prod)"
+  type        = string
+}
