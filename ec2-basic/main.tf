23318850508e4b87c7ce017f30c05fdef009bdbf
Nimrod Jocsan Trejo Castro <nimo-moto@Mac-mini-de-Nimrod.local>
2025-01-20 19:33:35 -0600

    main

--- ec2-basic/main.tf
+++ ec2-basic/main.tf
@@ -1,7 +1,7 @@
 module "ec2" {
   source          = "./modules/ec2_instance"
-#  environment     = var.environment
-#  ami_id          = var.ami_id
-#  instance_type   = var.instance_type
-#  instance_count  = var.instance_count
+  environment     = var.environment
+  ami_id          = var.ami_id
+  instance_type   = var.instance_type
+  instance_count  = var.instance_count
 }
